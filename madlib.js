646cb1bb5410a5deb9082d88c50580608f49cf44
Micah-Black <117412390+Micah-Black@users.noreply.github.com>
2022-11-14 20:01:23 -0600

    Create madlib.js

---
+++ madlib.js
@@ -0,0 +1,10 @@
+function results()
+{
+//Define the variables
+var adj = document.getElementById("adj").value;
+var place = document.getElementById("place").value;
+var pet = document.getElementById("pet").value;
+var adv = document.getElementById("adv").value;
+//Show results
+  alert("Once upon a time there was a " +adj+ " princess. She loved to walk all over the place. One day she walked all the way to " +place+ " with her " +pet+ " companion. Once they arrived they walked " +adv+ " all the way back home.")
+  }
